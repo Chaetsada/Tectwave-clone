abdae5b14c867d1c1ebc1d5bff955e6b3f858bf1
JKnochu <chaetsada.k@outlook.com>
2024-04-21 17:56:07 +0700

    edit : actions folder

--- src/app/actions/getArticles.ts
+++ src/app/actions/getArticles.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs'
 
 
 export default async function getArticles() {
-    const jsonData = await fs.readFile(process.cwd() + '/src/app/data/articles.json', 'utf8');
+    const jsonData = await fs.readFile(process.cwd() + '/app/data/articles.json', 'utf8');
     const data = JSON.parse(jsonData);
     return data;
 }
