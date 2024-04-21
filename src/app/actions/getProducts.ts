d712881b09212bca32146c8ee532ffa8de32316e
JKnochu <chaetsada.k@outlook.com>
2024-04-21 18:13:42 +0700

    edit path to json file

--- src/app/actions/getProducts.ts
+++ src/app/actions/getProducts.ts
@@ -2,7 +2,7 @@
 import { promises as fs } from 'fs'
 
 export default async function getProducts() {
-    const jsonData = await fs.readFile(process.cwd() + '/app/data/products.json', 'utf8');
+    const jsonData = await fs.readFile(process.cwd() + 'src/app/data/products.json', 'utf8');
     const data = JSON.parse(jsonData);
     return data;
 
